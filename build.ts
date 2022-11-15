0f8ead25933aba1202777bd3fe97d06d09819e66
Craig Doremus <cdoremus@gmail.com>
2022-11-15 10:30:50 -0500

    Updated builder.ignore args

--- build.ts
+++ build.ts
@@ -3,12 +3,17 @@ import { createBuilder } from "ultra/build.ts";
 const builder = createBuilder({
   browserEntrypoint: import.meta.resolve("./client.tsx"),
   serverEntrypoint: import.meta.resolve("./server.tsx"),
+  // inline dynamic import modules for Deno Deploy support
   inlineServerDynamicImports: true,
 });
 
 builder.ignore([
+  "./.git/**",
+  "./.vscode/**",
+  "./.github/**",
   "./README.md",
   "./importMap.json",
+  "./.gitignore",
 ]);
 
 // deno-lint-ignore no-unused-vars
